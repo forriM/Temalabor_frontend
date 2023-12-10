23e71ec80b2f2fd8e43aa16da1c36b71ebce691a
joel-megre <megrelishvilijoel@gmail.com>
2023-12-10 22:36:44 +0100

    dashboard change

--- frontend/src/app/professordashboard/professordashboard.component.ts
+++ frontend/src/app/professordashboard/professordashboard.component.ts
@@ -3,11 +3,13 @@ import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import {MatToolbar, MatToolbarModule} from '@angular/material/toolbar';
 import { LOCALSTORAGE_TOKEN_KEY } from '../app.component';
 import { AuthService } from '../auth.service';
+import {MatIconModule} from '@angular/material/icon'
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-professordashboard',
   standalone: true,
-  imports: [MatToolbarModule, RouterOutlet, RouterModule],
+  imports: [MatToolbarModule, RouterOutlet, RouterModule,MatIconModule,MatButtonModule],
   templateUrl: './professordashboard.component.html',
   styleUrl: './professordashboard.component.scss'
 })
--- frontend/src/app/studentdashboard/studentdashboard.component.ts
+++ frontend/src/app/studentdashboard/studentdashboard.component.ts
@@ -3,11 +3,13 @@ import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import {MatToolbar, MatToolbarModule} from '@angular/material/toolbar';
 import { LOCALSTORAGE_TOKEN_KEY } from '../app.component';
 import { AuthService } from '../auth.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-studentdashboard',
   standalone: true,
-  imports: [MatToolbarModule, RouterOutlet, RouterModule],
+  imports: [MatToolbarModule, RouterOutlet, RouterModule ,MatIconModule,MatButtonModule],
   templateUrl: './studentdashboard.component.html',
   styleUrl: './studentdashboard.component.scss'
 })
